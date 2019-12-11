7377e49700edb1b7c0732c4e793e1ff2923e7ca0
wanmaoor <wanmao1284879375@outlook.com>
2019-12-11 14:23:00 +0800

    Vue Instance
    Dynamic Arguments in Vue directives

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import "./styles.css";
-let data = { a: 1 };
 let vm = new Vue({
   el: "#app",
-  data: data
-});
-
-console.log(vm.$data === data);
-console.log(vm.$el === document.querySelector("#app"));
-
-vm.$watch("a", (newValue, oldValue) => {
-  console.log(newValue);
-  console.log(oldValue);
+  template: `<div>
+    <p>hello</p>
+    <a :[attributeName]="url">link</a>
+  </div>`,
+  data: {
+    attributeName: "href",
+    url: "https://baidu.com"
+  }
 });
