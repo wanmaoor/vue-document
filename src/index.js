b017baebde80b40150d0f36d4aac6959beb38c81
wanmaoor <wanmao1284879375@outlook.com>
2019-12-11 13:11:17 +0800

    Vue Instance
    ue instances expose a number of useful instance properties and methods.

--- src/index.js
+++ src/index.js
@@ -2,18 +2,13 @@ import "./styles.css";
 let data = { a: 1 };
 let vm = new Vue({
   el: "#app",
-  template: "<div>hello</div>",
   data: data
 });
 
-console.log(data.a == vm.a);
+console.log(vm.$data === data);
+console.log(vm.$el === document.querySelector("#app"));
 
-vm.a = 2;
-console.log(data.a);
-
-data.a = 3;
-console.log(vm.a);
-
-vm.b = "3";
-
-console.log(data.b);
+vm.$watch("a", (newValue, oldValue) => {
+  console.log(newValue);
+  console.log(oldValue);
+});
