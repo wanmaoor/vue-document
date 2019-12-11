8102025796fcc5413cdecec1a68e6260f94bb516
wanmaoor <wanmao1284879375@outlook.com>
2019-12-11 15:01:47 +0800

    Computed Property
    展示computed 与 methods的区别

--- src/index.js
+++ src/index.js
@@ -2,14 +2,34 @@ import "./styles.css";
 let vm = new Vue({
   el: "#app",
   data: {
-    message: "hello"
+    message: "hello",
+    n: 0
+  },
+  methods: {
+    reversedMessage() {
+      console.log("methods被调用");
+      return (
+        "methods:" +
+        this.message
+          .split("")
+          .reverse()
+          .join("")
+      );
+    },
+    add() {
+      this.n += 1;
+    }
   },
   computed: {
-    reversedMessage() {
-      return this.message
-        .split("")
-        .reverse()
-        .join("");
+    computedReversedMessage() {
+      console.log("computed 被调用");
+      return (
+        "computed:" +
+        this.message
+          .split("")
+          .reverse()
+          .join("")
+      );
     }
   }
 });
