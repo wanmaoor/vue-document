953fead482d2ac7e6d9f2dfa676b6351d89f7393
wanmaoor <wanmao1284879375@outlook.com>
2019-12-11 14:49:02 +0800

    Computed Properties

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import "./styles.css";
 let vm = new Vue({
   el: "#app",
-  template: `<div>
-    <p>hello</p>
-    <a :[attributeName]="url">link</a>
-  </div>`,
   data: {
-    attributeName: "href",
-    url: "https://baidu.com"
+    message: "hello"
+  },
+  computed: {
+    reversedMessage() {
+      return this.message
+        .split("")
+        .reverse()
+        .join("");
+    }
   }
 });
