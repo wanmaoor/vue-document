05fa025d63a52d85748bbc74ee722381d34d4a23
wanmaoor <wanmao1284879375@outlook.com>
2019-12-12 14:14:03 +0800

    Component Basic
    slot

--- src/index.js
+++ src/index.js
@@ -1,37 +1,26 @@
 import "./styles.css";
-Vue.component("blog-post", {
-  props: ["title"],
+Vue.component("alert-box", {
   template: `
-     <div>
-      <h1>{{title}}</h1>
-      <button @click="$emit('enlarge')">enlarge text</button>
-      <button @click="$emit('dwindle')">dwindle text</button>
-     </div>
-  `
-});
-new Vue({
-  el: "#app",
-  template: `
-    <div :style="{fontSize: postFontSize + 'em'}">
-      <blog-post 
-        v-for="post in posts"
-        :key="post.id" 
-        :title="post.title"
-        @enlarge="postFontSize+=0.1"
-        @dwindle="postFontSize-=0.1"
-      >
-        {{post.title}}
-      </blog-post>
+    <div :style="styles">
+      <strong>Error!</strong>
+      <slot></slot>
     </div>
   `,
   data() {
     return {
-      posts: [
-        { id: 1, title: "My journey with Vue" },
-        { id: 2, title: "Blogging with Vue" },
-        { id: 3, title: "Why Vue is so fun" }
-      ],
-      postFontSize: 1
+      styles: {
+        boder: "red",
+        background: "#cc3e44",
+        height: "40px",
+        width: "100%"
+      }
     };
   }
+});
+
+new Vue({
+  el: "#app",
+  template: `
+    <alert-box> what fuck?? </alert-box>
+  `
 });
