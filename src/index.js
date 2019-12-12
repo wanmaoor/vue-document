5eee58360d6c9d9703c4302e9495339dc4ea802b
wanmaoor <wanmao1284879375@outlook.com>
2019-12-12 13:39:18 +0800

    Component Basic
    first component

--- src/index.js
+++ src/index.js
@@ -1,46 +1,21 @@
 import "./styles.css";
-let vm = new Vue({
+new Vue({
   el: "#app",
-  data: {
-    message: "hello",
-    n: 0
+  template: `
+    <div>
+    <button-counter />
+    <button-counter />
+    <button-counter />
+    </div>
+  `
+});
+Vue.component("button-counter", {
+  data() {
+    return {
+      count: 0
+    };
   },
-  methods: {
-    reversedMessage() {
-      console.log("methods被调用");
-      return (
-        "methods:" +
-        this.message
-          .split("")
-          .reverse()
-          .join("")
-      );
-    },
-    add() {
-      this.n += 1;
-    },
-    wanmao() {
-      this.computedReversedMessage = "wanmao";
-    }
-  },
-  computed: {
-    computedReversedMessage: {
-      get() {
-        console.log("computed 被调用");
-        return (
-          "computed:" +
-          this.message
-            .split("")
-            .reverse()
-            .join("")
-        );
-      },
-      set(newValue) {
-        this.message = newValue
-          .split("")
-          .reverse()
-          .join("");
-      }
-    }
-  }
+  template: `
+    <button @click="count++">You click me {{count}} times</button>
+  `
 });
