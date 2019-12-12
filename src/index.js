95376ad3e2947c2a1ce3713861e6d94706749f2e
wanmaoor <wanmao1284879375@outlook.com>
2019-12-12 14:05:13 +0800

    Component Basic
    components communication

--- src/index.js
+++ src/index.js
@@ -2,22 +2,36 @@ import "./styles.css";
 Vue.component("blog-post", {
   props: ["title"],
   template: `
-    <h1>{{title}}</h1>
+     <div>
+      <h1>{{title}}</h1>
+      <button @click="$emit('enlarge')">enlarge text</button>
+      <button @click="$emit('dwindle')">dwindle text</button>
+     </div>
   `
 });
 new Vue({
   el: "#app",
   template: `
-    <div>
-      <blog-post v-for="post in posts" :key="post.id" v-text="post.title" />
-      
+    <div :style="{fontSize: postFontSize + 'em'}">
+      <blog-post 
+        v-for="post in posts"
+        :key="post.id" 
+        :title="post.title"
+        @enlarge="postFontSize+=0.1"
+        @dwindle="postFontSize-=0.1"
+      >
+        {{post.title}}
+      </blog-post>
     </div>
   `,
-  data: {
-    posts: [
-      { id: 1, title: "My journey with Vue" },
-      { id: 2, title: "Blogging with Vue" },
-      { id: 3, title: "Why Vue is so fun" }
-    ]
+  data() {
+    return {
+      posts: [
+        { id: 1, title: "My journey with Vue" },
+        { id: 2, title: "Blogging with Vue" },
+        { id: 3, title: "Why Vue is so fun" }
+      ],
+      postFontSize: 1
+    };
   }
 });
