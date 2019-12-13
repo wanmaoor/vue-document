4b1d71166a8ca60a13baaf39fd03efbf079a4087
wanmaoor <wanmao1284879375@outlook.com>
2019-12-13 17:38:28 +0800

    Slot
    multiple scoped slots

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ Vue.component("current-user", {
   template: `
   <span>
     <slot :user="user">{{ user.lastName }}</slot>
+    <slot :user="user" name="other">{{ user.other }}</slot>
   </span>
   `,
   data() {
     return {
       user: {
         firstName: "wan",
-        lastName: "fallback content"
+        lastName: "fallback content",
+        other: "other"
       }
     };
   }
@@ -17,8 +19,13 @@ Vue.component("current-user", {
 new Vue({
   el: "#app",
   template: `
-  <current-user v-slot:default="slotProps">
+  <current-user>
+   <template v-slot="slotProps">
     {{slotProps.user.firstName}}
+   </template>
+   <template v-slot:other="otherSlotProps">
+    {{otherSlotProps.user.other}}
+   </template>
   </current-user>
   `
 });
