eead4c41826c14cff09340a8e29d8d90734d7d5b
wanmaoor <wanmao1284879375@outlook.com>
2019-12-13 12:13:47 +0800

    Event Handle
    simple event handle

--- src/index.js
+++ src/index.js
@@ -1,26 +1,16 @@
 import "./styles.css";
-Vue.component("alert-box", {
-  template: `
-    <div :style="styles">
-      <strong>Error!</strong>
-      <slot></slot>
-    </div>
-  `,
-  data() {
-    return {
-      styles: {
-        boder: "red",
-        background: "#cc3e44",
-        height: "40px",
-        width: "100%"
-      }
-    };
-  }
-});
 
 new Vue({
   el: "#app",
   template: `
-    <alert-box> what fuck?? </alert-box>
-  `
+    <div>
+      <p>{{count}}</p>
+      <button @click="count ++">+1</button>
+    </div>
+  `,
+  data() {
+    return {
+      count: 0
+    };
+  }
 });
