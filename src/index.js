06628e127b73350b06bbe83b9cdc159baeec3823
wanmaoor <wanmao1284879375@outlook.com>
2019-12-15 12:57:45 +0800

    Render Function
    Basic

--- src/index.js
+++ src/index.js
@@ -1,47 +1,18 @@
-const mixIns = {
-  created() {
-    console.log("mixin is awesome");
+Vue.component("Title", {
+  render(h) {
+    return h("h" + this.level, this.$slots.default);
   },
-  data() {
-    return {
-      protoData: 1
-    };
-  },
-  methods: {
-    add() {
-      this.protoData += 1;
+  props: {
+    level: {
+      type: Number,
+      required: true
     }
   }
-};
-
-Vue.component("foo", {
-  template: `
-    <div>
-      foo
-      <p>{{protoData}}</p>
-      <button @click="add">foo +1</button>
-    </div>
-  `,
-  mixins: [mixIns]
-});
-
-Vue.component("bar", {
-  template: `
-    <div>
-      foo
-      <h1>{{protoData}}</h1>
-      <button @click="add">bar +1</button>
-    </div>
-  `,
-  mixins: [mixIns]
 });
 
 new Vue({
   el: "#app",
   template: `
-    <div>
-      <foo />
-      <bar />
-    </div>
+    <Title :level="1">hello world</Title>
   `
 });
