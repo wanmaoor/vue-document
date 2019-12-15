71a4f5d82da00b0a39e68c77b569806031c18f1a
wanmaoor <wanmao1284879375@outlook.com>
2019-12-15 17:38:55 +0800

    组件异步更新

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
-Vue.component("Title", {
-  render(h) {
-    return h("h" + this.level, this.$slots.default);
-  },
-  props: {
-    level: {
-      type: Number,
-      required: true
-    }
-  }
-});
-
 new Vue({
   el: "#app",
   template: `
-    <Title :level="1">hello world</Title>
-  `
+    <div id="example">
+      {{message}}
+      <button @click="updateMessage">更新</button>
+    </div>
+  `,
+  data() {
+    return {
+      message: "not updated"
+    };
+  },
+  methods: {
+    updateMessage() {
+      this.message = "updated";
+      this.message = "updated1";
+      this.message = "updated2";
+      this.message = "updated3";
+      // console.log(this.$el.textContent);
+      this.$nextTick(function() {
+        console.log(this.$el.textContent);
+      });
+    }
+  }
 });
