51b24ccf20507ba3078b7d853a5bf6aa05fe362a
wanmaoor <wanmao1284879375@outlook.com>
2019-12-19 18:45:22 +0800

    Computed
    使用计算属性分别男女

--- src/index.js
+++ src/index.js
@@ -1,26 +1,45 @@
+let index = 0;
+const createUser = (name, gender) => {
+  return { id: index++, name, gender };
+};
 new Vue({
   el: "#app",
   template: `
-    <div id="example">
-      {{message}}
-      <button @click="updateMessage">更新</button>
+    <div>
+      <button @click="setGender('')">全部</button>
+      <button @click="setGender('male')">男</button>
+      <button @click="setGender('female')">女</button>
+      <ul>
+        <li v-for="user in displayUsers" :key="user.id">
+          {{user.name}} - {{user.gender}}
+        </li>
+      </ul>
     </div>
   `,
   data() {
     return {
-      message: "not updated"
+      users: [
+        createUser("Alan", "male"),
+        createUser("Alice", "female"),
+        createUser("Bob", "male"),
+        createUser("Daisy", "female")
+      ],
+      gender: ""
     };
   },
+  computed: {
+    displayUsers() {
+      const { gender, users } = this;
+      if (gender === "") {
+        return users;
+      } else {
+        return users.filter(user => user.gender === gender);
+      }
+    }
+  },
   methods: {
-    updateMessage() {
-      this.message = "updated";
-      this.message = "updated1";
-      this.message = "updated2";
-      this.message = "updated3";
-      // console.log(this.$el.textContent);
-      this.$nextTick(function() {
-        console.log(this.$el.textContent);
-      });
+    setGender(gender) {
+      this.gender = gender;
     }
   }
 });
