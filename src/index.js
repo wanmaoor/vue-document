aaa81f2ff19ba3aa130ed14b4b7cf2bb27a8204c
wanmaoor <wanmao1284879375@outlook.com>
2019-12-25 18:42:51 +0800

    修饰符
    .sync

--- src/index.js
+++ src/index.js
@@ -1,45 +1,21 @@
-let index = 0;
-const createUser = (name, gender) => {
-  return { id: index++, name, gender };
-};
+Vue.component("Son", {
+  template: `
+   <div>
+   <p>{{count}}</p>
+   <button @click="$emit('update:count', count+1)">+1</button>
+   </div>
+  `,
+  props: ["count"]
+});
+
 new Vue({
   el: "#app",
-  template: `
-    <div>
-      <button @click="setGender('')">全部</button>
-      <button @click="setGender('male')">男</button>
-      <button @click="setGender('female')">女</button>
-      <ul>
-        <li v-for="user in displayUsers" :key="user.id">
-          {{user.name}} - {{user.gender}}
-        </li>
-      </ul>
-    </div>
-  `,
   data() {
     return {
-      users: [
-        createUser("Alan", "male"),
-        createUser("Alice", "female"),
-        createUser("Bob", "male"),
-        createUser("Daisy", "female")
-      ],
-      gender: ""
+      n: 1
     };
   },
-  computed: {
-    displayUsers() {
-      const { gender, users } = this;
-      if (gender === "") {
-        return users;
-      } else {
-        return users.filter(user => user.gender === gender);
-      }
-    }
-  },
-  methods: {
-    setGender(gender) {
-      this.gender = gender;
-    }
-  }
+  template: `
+    <Son :count.sync="n" />
+  `
 });
